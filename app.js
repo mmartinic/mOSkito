ffca3bca88770321a9c19786f05f59392b34c72b
Marin Martinic <corax34@gmail.com>
2015-10-10 14:02:14 +0200

    add heroku support

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var restify = require('restify');
 
 var server = restify.createServer();
+var port = process.env.PORT || 8080;
 
 require('./routes')(server);
 
-server.listen(8080, function() {
+server.listen(port, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
