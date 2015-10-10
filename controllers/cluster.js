bb5c50a8064d984fccf5d0f2810e750a35c0762b
Marin Martinic <corax34@gmail.com>
2015-10-10 21:46:00 +0200

    fix calculate cluster radius bug

--- controllers/cluster.js
+++ controllers/cluster.js
@@ -3,6 +3,7 @@ var ClusterModel = require('../models/cluster');
 var BiteService = require('../services/bite_service');
 var geocluster = require("geocluster");
 var _ = require('lodash');
+var Stats = require('fast-stats').Stats;
 
 ClusterController.getClusters = function getClusters(req, res, next) {
     BiteService.getBites(10000).then(function (data) {
@@ -10,10 +11,20 @@ ClusterController.getClusters = function getClusters(req, res, next) {
         var clusters = geocluster(_.map(data.results, function(bite) {
             return [bite.geo.latitude, bite.geo.longitude];
         }), bias);
+
         var response = _.map(clusters, function (cluster) {
-            var radius = _.max(_.map(cluster.elements, function(element){
-                return dist(element[0], cluster.centroid[0], element[1], cluster.centroid[1])
-            }));
+            var distanceSet = new Stats();
+
+            _.map(cluster.elements, function(element){
+                return dist(element[0], element[1], cluster.centroid[0], cluster.centroid[1]);
+            }).forEach(function(distance) {
+                distanceSet.push(distance);
+            });
+
+            var distanceSetFiltered = distanceSet.iqr();
+
+            var radius = distanceSetFiltered.max;
+
             return ClusterModel.createCluster(cluster.centroid[0], cluster.centroid[1], radius, cluster.elements.length);
         });
 
@@ -38,6 +49,6 @@ function dist(lat1, lon1, lat2, lon2) {
     var dlon = (lon2 - lon1).toRad();
     var a = (Math.sin(dlat/2) * Math.sin(dlat/2) + Math.sin(dlon/2) * Math.sin(dlon/2) * Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()));
     return (Math.round(((2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))) * 6371)*100)/100);
-};
+}
 
 module.exports = ClusterController;
