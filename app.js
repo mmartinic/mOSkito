4c322b55e98296b1bcf363d357d7ab37ba88ada9
Mihovil Rister <mrister@rhapsody.com>
2015-10-11 04:50:14 +0200

    add websocket support headers.

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ server.use(restify.gzipResponse());
 server.use(restify.bodyParser());
 server.use(restify.fullResponse());
 
+server.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 require('./routes')(server, io);
 
 server.listen(port, function() {
