0a784ce415014aeb07c787c1a5b0ead50c686167
Mihovil Rister <mrister@rhapsody.com>
2015-10-11 05:56:58 +0200

    host socks.io

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var restify = require('restify');
 var config = require('./config/config.json');
+var fs = require('fs');
 var socketio = require('socket.io');
-
 
 var server = restify.createServer({name: config.server.name});
 var io = socketio.listen(server.server);
-io.set( {origins:'herokuapp.com:* http://herokuapp.com:* http://moskito-web.herokuapp.com/:* https://moskito-web.herokuapp.com/:*'});
+io.set({origins: 'herokuapp.com:* http://herokuapp.com:* http://moskito-web.herokuapp.com/:* https://moskito-web.herokuapp.com/:*'});
+io.set('transports', [
+  'websocket'
+  , 'flashsocket'
+  , 'htmlfile'
+  , 'xhr-polling'
+  , 'jsonp-polling'
+]);
 
 var port = process.env.PORT || config.server.port;
 
@@ -17,17 +24,27 @@ server.use(restify.gzipResponse());
 server.use(restify.bodyParser());
 server.use(restify.fullResponse());
 
-server.on( 'after',
-  function crossOrigin(req,res){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  }
-);
+server.get(/socket[.]io[.]js$/, function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  fs.readFile('socket.io.js', function (err, file) {
+    if (err) {
+      console.log(err);
+      res.send(500);
+      return next();
+    }
+
+    res.write(file);
+    res.end();
+    return next();
+  });
+
+});
 
 require('./routes')(server, io);
 
-server.listen(port, function() {
+server.listen(port, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
